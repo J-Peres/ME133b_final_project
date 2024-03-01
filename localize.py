5e34032282fbcec845f8c497965334bf04c7efc2
firdavsn <firdavsnasriddinov7@gmail.com>
2024-02-29 19:57:39 -0800

    updated lozalize.py files

--- localize.py
+++ localize.py
@@ -27,6 +27,8 @@ walls = m.grid
 rows  = np.size(walls, axis=0)
 cols  = np.size(walls, axis=1)
 
+print(walls)
+
 # generates start and end, if want them on outer wall its true
 m.generate_entrances(True, True)
 start = m.start
@@ -56,11 +58,20 @@ def computePrediction(bel, drow, dcol, probCmd = 1):
     prd = np.zeros((rows,cols))
 
     # Determine the new probablities (remember to consider walls).
-    for r in range((drow < 0), rows - (drow > 0)):
-        for c in range((dcol < 0), cols - (dcol > 0)):
-            prd[r, c] += walls[r+drow, c+dcol] * bel[r, c]
-            prd[r+drow, c+dcol] += (1 - walls[r+drow, c+dcol]) * bel[r, c]
-
+    for r in range(1, rows-1):
+        for c in range(1, cols-1):
+            new_r = r + drow
+            new_c = c + dcol
+            
+            # If not colliding with a wall
+            if walls[new_r, new_c] == 0:
+                prd[new_r, new_c] += probCmd * bel[r, c]
+                
+                # Account for probability of command not executing
+                prd[r, c] += (1 - probCmd) * bel[r, c]
+            else:
+                prd[r, c] += probCmd * bel[r, c]
+    
     # Return the prediction grid
     return prd
 
@@ -75,9 +86,15 @@ def computePrediction(bel, drow, dcol, probCmd = 1):
 #
 def updateBelief(prior, probSensor, sensor):
     # Create the posterior belief.
-    mult = probSensor if sensor else 1 - probSensor
-    post = np.multiply(prior, mult)
+    post = np.zeros((rows, cols))
+    for r in range(rows):
+        for c in range(cols):
+            if sensor:
+                post[r, c] = probSensor[r, c] * prior[r, c]
+            else:
+                post[r, c] = (1 - probSensor[r, c]) * prior[r, c]
 
+    
     # Normalize.
     s = np.sum(post)
     if (s == 0.0):
@@ -99,9 +116,31 @@ def precomputeSensorProbability(drow, dcol, probProximal = [1.0]):
     prob = np.zeros((rows, cols))
 
     # Pre-compute the sensor probability on the grid.
-    for r in range(1, rows-1):
-        for c in range(1, cols-1):
-            prob[r, c] = walls[r + drow, c + dcol]
+    for r in range(rows):
+        for c in range(cols):
+            for i, prox in enumerate(probProximal):
+                dist = i + 1
+                
+                new_r = r + dist * drow
+                new_c = c + dist * dcol
+                
+                # If within grid and sensor sees walls at distance dist
+                if 0 <= new_r < rows and 0 <= new_c < cols and walls[new_r, new_c] == 1:
+                    prob[r, c] += prox
+    
+    # Ensures that no cell has zero belief by subtracting 5 percent of belief from top 10 cells with 
+    # highest sensor probability
+    percent = .05
+    k = 10
+    top_flat = np.argpartition(prob.flatten(), -k)[-k:]
+    top_indices = np.array(np.unravel_index(top_flat[np.argsort(-prob.flatten()[top_flat])], prob.shape)).T
+    total_nonzero_prob = 0
+    for (r, c) in top_indices:
+        total_nonzero_prob += prob[r, c] * percent
+    nonzero_prob = total_nonzero_prob / np.count_nonzero(prob == 0)
+    for r in range(rows):
+        for c in range(cols):
+            if prob[r, c] == 0: prob[r, c] += nonzero_prob
 
     # Return the computed grid.
     return prob
@@ -114,7 +153,7 @@ def main():
     # FIXME... PICK WHAT THE "REALITY" SHOULD SIMULATE:
     # Initialize the robot simulation.
     # robot=Robot(walls) # Prob 1(a)
-    robot=Robot(walls, row=start[0], col=start[1]) # Prob 1(b) !!!!!!6 is shortest path!!!!!!!!
+    # robot=Robot(walls, row=start[0], col=start[1]) # Prob 1(b) !!!!!!6 is shortest path!!!!!!!!
     # Prob 2     robot=Robot(walls, row=12, col=26, probProximal=[0.9,0.6,0.3])
     # Prob 3     robot=Robot(walls, row=15, col=47, probProximal=[0.9,0.6,0.3],
     #                        probCmd=0.8)
@@ -124,8 +163,10 @@ def main():
 
 
     # Initialize your localization parameters.
-    probCmd      = 1 # FIXME... PICK WHAT YOUR LOCALIZATION SHOULD ASSUME
-    probProximal = [1.0] # FIXME... PICK WHAT YOUR LOCALIZATION SHOULD ASSUME
+    probCmd      = 1
+    probProximal = [0.9]
+    
+    robot = Robot(walls, row=start[0], col=start[1], probProximal=[0.9, 0.6, 0.3], probCmd=probCmd)
 
     # Report.
     print("Localization is assuming probProximal = " + str(probProximal) +
